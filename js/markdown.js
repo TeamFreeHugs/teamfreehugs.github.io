5e5d4c4b85f2aedb98e88b649676deec5919e01c
Eyeballcode <edwardyeung.private@gmail.com>
2015-07-20 15:35:56 +0800

    Automated Commit Script

--- js/markdown.js
+++ js/markdown.js
@@ -1,11 +1,12 @@
 function load() {
 	var quotes = document.getElementsByTagName('quote');
-	quotes.foreach(function(entry) {
-		var quote= entry.innerHTML;
+	for (var i = 0; i < quotes.length; ++i) {
+		var entry = quotes[i];
+		var quote = entry.innerHTML;
 		var newQuote = document.createElement('blockquote');
 		newQuote.className = "quote";
 		newQuote.innerHTML = quote;
 		quote.parentElement.appendChild(newQuote);
 		quote.parentElement.removeChild(quote);
-	});
+	}
 }
