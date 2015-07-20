dd8f8b520517938a668aabb5a9db73dcc7b0c9d6
Eyeballcode <edwardyeung.private@gmail.com>
2015-07-20 16:32:19 +0800

    Automated Commit Script

--- js/markdown.js
+++ js/markdown.js
@@ -33,31 +33,30 @@ function sleep(millis) {
 
 (function() {
 	var head = document.getElementsByTagName('head')[0];
+	var jqueryScript = document.createElement('script');
+	jqueryScript.src = "http://code.jquery.com/jquery-1.11.3.min.js";
+	head.appendChild(jqueryScript);
 	var markdownCSS = document.createElement('link');
 	markdownCSS.href = "http://teamfreehugs.github.io/styles/markdown-styles.css";
 	markdownCSS.rel = "stylesheet";
 	markdownCSS.type = "text/css";
-	var jqueryScript = document.createElement('script');
 	var codeHighlighter = document.createElement('script');
 	var lua = document.createElement('script');
 	var css = document.createElement('script');
 	var code_style = document.createElement('link');
 	var notify = document.createElement('script');
-	jqueryScript.src = "http://code.jquery.com/jquery-1.11.3.min.js";
 	codeHighlighter.src = "https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js";
 	lua.src = "https://github.com/google/code-prettify/blob/master/src/lang-lua.js";
 	css.src = "https://github.com/google/code-prettify/blob/master/src/lang-css.js";
 	code_style.rel = "stylesheet";
 	code_style.href = "https://cdn.rawgit.com/google/code-prettify/master/styles/sons-of-obsidian.css";
 	code_style.type = "text/css";
-	notify.src = "http://notifyjs.com/dist/notify-combined.min.js";
+	notify.src = "http://notifyjs.com/dist/notify-combined.js";
 	head.appendChild(markdownCSS);
-	head.appendChild(jqueryScript);
 	head.appendChild(codeHighlighter);
 	head.appendChild(lua);
 	head.appendChild(css);
 	head.appendChild(code_style);
 	sleep(1000);
-	console.log('Adding notify...');
 	head.appendChild(notify);
 })();
