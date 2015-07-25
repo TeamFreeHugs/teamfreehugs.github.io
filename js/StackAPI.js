cd7ed8d89a6abfa558a1378d37f9da830f759af9
Eyeballcode <edwardyeung.private@gmail.com>
2015-07-25 22:56:40 +0800

    update

--- js/StackAPI.js
+++ js/StackAPI.js
@@ -1,4 +1,5 @@
 function StackAPI(credentials) {
+
 	this.clientID = credentials.clientID;
 	if (credentials.key != undefined) {
 		this.key = credentials.key;
@@ -12,24 +13,26 @@ function StackAPI(credentials) {
 		if (credentials.redirect_url != undefined) {
 			this.redirect_url = credentials.redirect_url;
 		}
-
-		this.authenticate = function() {
-			var authURL = 'https://stackexchange.com/oauth/dialog?client_id' = this.clientID
-					+ '&scope=';
-			for ( var pos = 0; pos < this.scope.length; ++pos) {
-				authURL += this.scope[pos] + '';
-				if (pos != this.scope.length)
-					authURL += ',';
-			}
-			authURL += '&redirect_uri=';
-			if (this.redirect_url != undefined) {
-				authURL += this.redirect_url;
-			} else {
-				authURL += window.location.toString();
-			}
-			if (credentials.state != undefined) {
-				authURL += '&state=' + credentials.state;
-			}
-		}
 	}
+
+	this.authenticate = function() {
+		var authURL = 'https://stackexchange.com/oauth/dialog?client_id' = this.clientID
+				+ '&scope=';
+		for ( var pos = 0; pos < this.scope.length; ++pos) {
+			authURL += this.scope[pos] + '';
+			if (pos != this.scope.length)
+				authURL += ',';
+		}
+		authURL += '&redirect_uri=';
+		if (this.redirect_url != undefined) {
+			authURL += this.redirect_url;
+		} else {
+			authURL += window.location.toString();
+		}
+		if (credentials.state != undefined) {
+			authURL += '&state=' + credentials.state;
+		}
+		alert(authURL);
+	}
+	return this;
 }
