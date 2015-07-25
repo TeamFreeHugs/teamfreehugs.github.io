673f409d0a2f8d74aa3898affbf075623f149167
Eyeballcode <edwardyeung.private@gmail.com>
2015-07-25 23:56:37 +0800

    Testing!!!

--- js/StackAPI.js
+++ js/StackAPI.js
@@ -1,5 +1,38 @@
-function StackAPI(credentials) {
+function getCode(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex
+			.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g,
+			" "));
+}
 
+function readTextFile(file, error) {
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", file, false);
+	rawFile.onreadystatechange = function() {
+		if (rawFile.readyState === 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
+				return rawFile.responseText;
+			}
+		}
+	}
+	rawFile.onerror = error;
+	try {
+		rawFile.send(null);
+	} catch (e) {
+		error();
+	}
+	return rawFile.responseText;
+}
+
+function pathExists(url) {
+	var http = new XMLHttpRequest();
+	http.open('HEAD', url, false);
+	http.send();
+	return http.status != 404;
+}
+
+function StackAPI(credentials) {
 	this.clientID = credentials.clientID;
 	if (credentials.key != undefined) {
 		this.key = credentials.key;
@@ -14,26 +47,31 @@ function StackAPI(credentials) {
 			this.redirect_url = credentials.redirect_url;
 		}
 	}
+	this.authenticate = function() {
+		this.authURL = 'https://stackexchange.com/oauth/dialog?client_id=';
+		this.authURL += this.clientID;
+		if (this.scopre != undefined) {
+			this.authURL + '&scope=';
+			for ( var pos = 0; pos < this.scope.length; ++pos) {
+				this.authURL += this.scope[pos] + '';
+				if (pos != this.scope.length)
+					this.authURL += ',';
+			}
+		}
+		this.authURL += '&redirect_uri=';
+		if (this.redirect_url != undefined) {
+			this.authURL += this.redirect_url;
+		} else {
+			this.authURL += window.location.toString();
+		}
+		if (credentials.state != undefined) {
+			this.authURL += '&state=' + this.credentials.state;
+		}
+		if (getCode('access_token') == "") {
+			window.location = this.authURL;
+		} else {
+			return getCode('access_token');
+		}
 
-	return this;
-}
-authenticate()
-{
-	var authURL = 'https://stackexchange.com/oauth/dialog?client_id' = this.clientID
-			+ '&scope=';
-	for ( var pos = 0; pos < this.scope.length; ++pos) {
-		authURL += this.scope[pos] + '';
-		if (pos != this.scope.length)
-			authURL += ',';
 	}
-	authURL += '&redirect_uri=';
-	if (this.redirect_url != undefined) {
-		authURL += this.redirect_url;
-	} else {
-		authURL += window.location.toString();
-	}
-	if (credentials.state != undefined) {
-		authURL += '&state=' + this.credentials.state;
-	}
-	alert(authURL);
 }
