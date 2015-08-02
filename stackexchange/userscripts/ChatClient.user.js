a946e1c084ed407b84bb5903bca9eb470b086927
eyeballcode <edwardyeung.private@gmail.com>
2015-08-02 21:52:12 +0800

    Update

--- stackexchange/userscripts/ChatClient.user.js
+++ stackexchange/userscripts/ChatClient.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Chat Client
-// @version      1.0
+// @version      1.1
 // @description  Adds in a chat button
 // @author       Uni*
 // @match        *://*.stackexchange.com/*
@@ -11,6 +11,7 @@
 // @match        *://*.serverfault.com/*
 // @match        *://*.askubuntu.com/*
 // @grant        none
+// @updateURL    http://teamfreehugs.github.io/stackexchange/userscripts/ChatClient.user.js
 // ==/UserScript==
 
 (function() {
@@ -21,9 +22,18 @@
 	l.href = "http://teamfreehugs.github.io/styles/stackexchange/chatclient.css";
 	head.appendChild(l);
 	var chat = document.createElement('a');
+	var chatTab = document.createElement('div');
+	
+	chatTab.className = "topbar-dialog chatDialog dno";
+	
 	chat.href = "javascript:void(0);";
 	chat.addEventListener("click", function() {
 	});
 	chat.className = "hover chatButton";
 	toolbar.appendChild(chat);
+	chat.addEventListener("click", function() {
+		console.log('Loading chat...');
+	});
+	var title = document.createElement('h3');
+	title.className = "header";
 })();
