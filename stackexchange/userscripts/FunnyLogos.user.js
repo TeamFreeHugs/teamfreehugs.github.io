be82552b09a41c592d88a5d9c239f4624a0bc939
Eyeballcode <edwardyeung.private@gmail.com>
2015-09-20 02:21:51 +0800

    Finish SO part of userscript

--- stackexchange/userscripts/FunnyLogos.user.js
+++ stackexchange/userscripts/FunnyLogos.user.js
@@ -24,12 +24,24 @@ withJquery(function go($) {
   $.getScript('http://teamfreehugs.github.io/js/URI.js', function (data, textStatus, jqxhr) {
     url = new URI(window.location.toString())
     logo = $('#hlogo > a')
+    star = $('.star-off')
     if (url.authority == 'stackoverflow.com') {
       logo.css({
         'background-image': 'url("http://teamfreehugs.github.io/img/stackoverflow.svg"), none',
         width: '250px',
         height: '80px'
       });
+      if (url.path.startsWith("/questions")) {
+        if (!(url.query == "tab=favorites" && url.path.startsWith("/users"))) {
+          $('.anonymous-gravatar').css({
+            'background-image': 'url("http://teamfreehugs.github.io/img/stackoverflow.svg"), none'
+          });
+        }
+        star.css({
+          'background-image' : 'url("http://teamfreehugs.github.io/img/stackoverflow.svg"), none'
+        });
+
+      }
     }
     $('#hlogo > a').css({display:"block"})
   })
