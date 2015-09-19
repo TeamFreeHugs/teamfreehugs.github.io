3e83116b74b636d10d48dd856722a7231165f650
Eyeballcode <edwardyeung.private@gmail.com>
2015-09-19 08:43:21 +0800

    Works

--- stackexchange/userscripts/FunnyLogos.user.js
+++ stackexchange/userscripts/FunnyLogos.user.js
@@ -10,25 +10,27 @@
 // @match        *://superuser.com/*
 // @match        *://serverfault.com/*
 // @match        *://askubuntu.com/*
+// @run-at       document-end
 // @grant        none
 // ==/UserScript==
-
-function addScripts() {
-	var head = document.head;
-	var uri = document.createElement('script');
-	uri.src = "https://teamfreehugs.github.io/js/URI.js";
-	head.appendChild(uri);
+function withJquery(f) {
+  s = document.createElement('script');
+  s.textContent = '(' + f.toString() + ')(jQuery)'
+  document.head.appendChild(s)
 }
 
-window.onload = function() {
-  addScripts()
-  url = new URI(window.location.toString())
-  logo = $('#hlogo > a')
-  if (url.authority == "stackoverflow.com") {
-    logo.css({
-        "background-image" : "url(\"http://teamfreehugs.github.io/img/stackoverflow.svg\"), none",
-        width : "250px",
-        height : "80px"
-    });
-  }
-}
+$('#hlogo > a').css({display:"none"})
+withJquery(function go($) {
+  $.getScript('http://teamfreehugs.github.io/js/URI.js', function (data, textStatus, jqxhr) {
+    url = new URI(window.location.toString())
+    logo = $('#hlogo > a')
+    if (url.authority == 'stackoverflow.com') {
+      logo.css({
+        'background-image': 'url("http://teamfreehugs.github.io/img/stackoverflow.svg"), none',
+        width: '250px',
+        height: '80px'
+      });
+    }
+    $('#hlogo > a').css({display:"block"})
+  })
+});
