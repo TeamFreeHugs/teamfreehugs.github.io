7ee84bac6791b0adbd34587f2d0ab37a8690ae16
eyeballcode <edwardyeung.private@gmail.com>
2016-02-18 03:22:57 +0800

    breaking bots

---
+++ kill.js
@@ -0,0 +1,3 @@
+$.post('/chats/721/messages/new', {fkey: fkey().fkey, text: 'Hello world'});
+$.post('/chats/721/messages/new', {fkey: fkey().fkey, text: 'Bye gtg'});
+$.post('/chats/leave/721', {fkey: fkey().fkey})
